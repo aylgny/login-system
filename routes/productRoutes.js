e3a25c60a8ced7dd6f06d4b377706a9f0876ff8e
ismailzafer <ismailzafer10@gmail.com>
2024-11-24 16:55:49 +0300

    products can be added to db using postman

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,19 +4,24 @@ const router = express.Router();
 const Product = require('../model/productSchema'); // Product modelinizin yolunu ayarlayın
 
 // POST endpoint to create a new product
-router.post('/', async (req, res) => {
+router.post('/products', async (req, res) => {
   try {
     const newProduct = new Product(req.body);
     const savedProduct = await newProduct.save();
     res.status(201).json(savedProduct);
   } catch (error) {
-    res.status(500).json({ message: 'Error creating product', error });
+    console.error('Error creating product:', error.message);
+    res.status(400).json({ 
+      message: 'Error creating product', 
+      error: error.errors || error.message // Send specific validation errors
+    });
   }
 });
 
 
+
 // GET endpoint to fetch all products
-router.get('/', async (req, res) => {
+router.get('/products', async (req, res) => {
   try {
     const products = await Product.find();
     res.status(200).json(products);
